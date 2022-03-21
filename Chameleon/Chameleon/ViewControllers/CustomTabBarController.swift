35528fa2b4db8db8dc4cac3b85e9a8d9adf06112
유정주 <dbwjdwn9216@gmail.com>
2022-03-21 23:14:54 +0900

    Update: 탭바 컬러 변경

--- Chameleon/Chameleon/ViewControllers/CustomTabBarController.swift
+++ Chameleon/Chameleon/ViewControllers/CustomTabBarController.swift
@@ -14,15 +14,15 @@ class CustomTabBarController: UITabBarController {
         
         let mainVC = UINavigationController(rootViewController: MainViewController())
         mainVC.tabBarItem.image = UIImage(named: "HomeTabIcon")
-//        mainVC.tabBarItem.selectedImage = UIImage(named: "HomeTabSelectedIcon")
         mainVC.tabBarItem.title = "Home"
         
         let moreVC = UINavigationController(rootViewController: MoreViewController())
         moreVC.tabBarItem.image = UIImage(named: "MoreTabIcon")
-//        moreVC.tabBarItem.selectedImage = UIImage(named: "MoreTabSelectedIcon")
         moreVC.tabBarItem.title = "More"
         
         self.tabBar.tintColor = UIColor(named: "MainColor")
+        
+        self.tabBar.backgroundColor = UIColor(named:)
         
         self.viewControllers = [mainVC, moreVC]
     }
