2ddc8a10609d92ae660f37dcf6f614a10f79f7bc
유정주 <dbwjdwn9216@gmail.com>
2022-03-21 23:45:03 +0900

    Fix: 코드 리팩토링

--- Chameleon/Chameleon/ViewControllers/CustomTabBarController.swift
+++ Chameleon/Chameleon/ViewControllers/CustomTabBarController.swift
@@ -9,9 +9,17 @@ import UIKit
 import SnapKit
 
 class CustomTabBarController: UITabBarController {
+ 
+    //MARK: - Life Cycles
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        initViewControllers()
+        setUpTabBar()
+    }
+    
+    //MARK: - Methods
+    private func initViewControllers() {
         let mainVC = UINavigationController(rootViewController: MainViewController())
         mainVC.tabBarItem.image = UIImage(named: "HomeTabIcon")
         mainVC.tabBarItem.title = "Home"
@@ -20,16 +28,19 @@ class CustomTabBarController: UITabBarController {
         moreVC.tabBarItem.image = UIImage(named: "MoreTabIcon")
         moreVC.tabBarItem.title = "More"
         
+        self.viewControllers = [mainVC, moreVC]
+    }
+    
+    private func setUpTabBar() {
+        self.tabBar.clipsToBounds = true
+        
         self.tabBar.tintColor = UIColor(named: "MainColor")
         self.tabBar.backgroundColor = UIColor(named:"TabBarColor")
         
         self.tabBar.layer.borderWidth = 3
         self.tabBar.layer.borderColor = UIColor(named: "TabBarBorderColor")?.cgColor
         
-        self.tabBar.clipsToBounds = true
         self.tabBar.layer.cornerRadius = 20
         self.tabBar.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
-        
-        self.viewControllers = [mainVC, moreVC]
     }
 }
