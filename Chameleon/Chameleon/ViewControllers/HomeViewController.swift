0a1d315be77b00153175db76045380dd7da30e05
유정주 <dbwjdwn9216@gmail.com>
2022-03-22 19:31:10 +0900

    Add: 홈 화면 UI 완료

--- Chameleon/Chameleon/ViewControllers/HomeViewController.swift
+++ Chameleon/Chameleon/ViewControllers/HomeViewController.swift
@@ -11,47 +11,45 @@ import SnapKit
 class HomeViewController: UIViewController {
     
     //MARK: - Views
-    lazy var photoButton: UIButton = {
-        let view = UIButton()
-        view.backgroundColor = .white
+    lazy var photoButton: UIView = {
+        let view = menuView()
         
-        view.layer.borderWidth = 1
-        view.layer.borderColor = UIColor(named: "ButtonBorderColor")?.cgColor
-        view.layer.cornerRadius = 15
-        
-        view.layer.shadowColor = UIColor.lightGray.cgColor
-        view.layer.shadowOpacity = 0.3
-        view.layer.shadowOffset = CGSize(width: 0, height: 2)
-        view.layer.shadowRadius = 3
-        
-        //inner view
-        let innerView = UIView()
+        let innerView = buttonInnerStackView(imageName: "PhotoButton", title: "PHOTO")
         view.addSubview(innerView)
         innerView.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.centerY.equalToSuperview().offset(8)
+        }
+        
+        let clickButton = UIButton()
+        clickButton.addTarget(self, action: #selector(touchDownPhotoButton(sender:)), for: .touchDown)
+        clickButton.addTarget(self, action: #selector(touchUpPhotoButton(sender:)), for: .touchUpInside)
+        
+        view.addSubview(clickButton)
+        clickButton.snp.makeConstraints { make in
             make.width.height.equalToSuperview()
         }
         
-        //image
-        let imageView = UIImageView()
-        if let buttonImage = UIImage(named: "PhotoButton") {
-            imageView.image = buttonImage
+        return view
+    }()
+    
+    lazy var videoButton: UIView = {
+        let view = menuView()
+        
+        let innerView = buttonInnerStackView(imageName: "VideoButton", title: "VIDEO")
+        view.addSubview(innerView)
+        innerView.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.centerY.equalToSuperview().offset(8)
         }
         
-        innerView.addSubview(imageView)
-        imageView.snp.makeConstraints { make in
-            make.width.height.equalTo(49)
-            make.centerX.equalToSuperview()
-            make.centerY.equalToSuperview().offset(-10)
-        }
+        let clickButton = UIButton()
+        clickButton.addTarget(self, action: #selector(touchDownVideoButton(sender:)), for: .touchDown)
+        clickButton.addTarget(self, action: #selector(touchUpVideoButton(sender:)), for: .touchUpInside)
         
-        //label
-        let textLabel = UILabel()
-        textLabel.text = "PHOTO"
-        
-        innerView.addSubview(textLabel)
-        textLabel.snp.makeConstraints { make in
-            make.centerX.equalToSuperview()
-            make.top.equalTo(imageView.snp.bottom).offset(10)
+        view.addSubview(clickButton)
+        clickButton.snp.makeConstraints { make in
+            make.width.height.equalToSuperview()
         }
         
         return view
@@ -66,11 +64,51 @@ class HomeViewController: UIViewController {
         setUpHomeUI()
     }
     
+    //MARK: - Actions
+    @objc func touchDownPhotoButton(sender: UIButton) {
+        photoButton.backgroundColor = UIColor(named: "MainColor")
+        changeSubviewsColor(view: photoButton, color: .white)
+    }
+    
+    @objc func touchUpPhotoButton(sender: UIButton) {
+        photoButton.backgroundColor = .white
+        changeSubviewsColor(view: photoButton, color: .black)
+    }
+    
+    @objc func touchDownVideoButton(sender: UIButton) {
+        videoButton.backgroundColor = UIColor(named: "MainColor")
+        changeSubviewsColor(view: videoButton, color: .white)
+    }
+    
+    
+    @objc func touchUpVideoButton(sender: UIButton) {
+        videoButton.backgroundColor = .white
+        changeSubviewsColor(view: videoButton, color: .black)
+    }
+    
     //MARK: - Methods
+    private func changeSubviewsColor(view: UIView, color: UIColor) {
+        for v in view.subviews {
+            if v is UIStackView {
+                let currentStackView = v as! UIStackView
+                for sv in currentStackView.subviews {
+                    if sv is UILabel {
+                        let currnetLabel = sv as! UILabel
+                        currnetLabel.textColor = color
+                    } else if sv is UIImageView {
+                        let currnetImageView = sv as! UIImageView
+                        currnetImageView.tintColor = color
+                    }
+                }
+            }
+        }
+    }
+    
     private func setUpNavigationBar() {
         navigationItem.title = "Face Swap with Fake Face"
         
         let appearance = UINavigationBarAppearance()
+        appearance.backgroundColor = .systemBackground
         navigationItem.standardAppearance = appearance
         navigationItem.scrollEdgeAppearance = appearance
         
@@ -84,38 +122,102 @@ class HomeViewController: UIViewController {
     private func setUpHomeUI() {
         view.backgroundColor = .systemBackground
         
-        view.addSubview(photoButton)
+        let buttonView = UIStackView(arrangedSubviews: [photoButton, videoButton])
+        buttonView.axis = .horizontal
+        buttonView.spacing = 20
+        buttonView.alignment = .center
+        buttonView.distribution = .fillEqually
+        
+        view.addSubview(buttonView)
+        
+        buttonView.snp.makeConstraints { make in
+            let width = view.frame.width * 0.8
+            make.width.equalTo(width)
+            make.height.equalTo(width * 0.35)
+            make.centerX.equalToSuperview()
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(40)
+        }
+        buttonView.addSubview(photoButton)
+        buttonView.addSubview(videoButton)
+
         photoButton.snp.makeConstraints { (make) in
-            make.width.equalTo(self.view.frame.width / 2.5)
-            make.height.equalTo(self.view.frame.width / 3.5)
-            make.top.equalTo(view.safeAreaLayoutGuide).offset(34)
-            make.left.equalTo(view.safeAreaLayoutGuide).offset(20)
+            make.height.equalToSuperview()
         }
         
+        videoButton.snp.makeConstraints { make in
+            make.height.equalToSuperview()
+        }
+    }
+    
+    private func buttonInnerStackView(imageName: String, title: String) -> UIStackView {
+        //inner view
+        let imageView = UIImageView()
+        let textLabel = UILabel()
+        let innerStackView = UIStackView(arrangedSubviews: [imageView, textLabel])
+
+        innerStackView.translatesAutoresizingMaskIntoConstraints = false
+        innerStackView.axis = .vertical
+        innerStackView.spacing = 15
+        innerStackView.distribution = .fill
+        innerStackView.alignment = .center
         
+        //image
+        if let buttonImage = UIImage(named: imageName) {
+            imageView.image = buttonImage.withRenderingMode(.alwaysTemplate)
+            imageView.tintColor = .black
+        }
+        imageView.snp.makeConstraints { make in
+            make.width.height.equalTo(40)
+            make.centerX.equalToSuperview()
+        }
+        
+        //label
+        textLabel.text = title
+        textLabel.font = UIFont.boldSystemFont(ofSize: 16)
+        textLabel.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+        }
+        
+        return innerStackView
+    }
+    
+    private func menuView() -> UIView {
+        let view = UIView()
+        view.backgroundColor = .white
+        
+        view.layer.borderWidth = 1
+        view.layer.borderColor = UIColor(named: "ButtonBorderColor")?.cgColor
+        view.layer.cornerRadius = 15
+        
+        view.layer.shadowColor = UIColor.lightGray.cgColor
+        view.layer.shadowOpacity = 0.3
+        view.layer.shadowOffset = CGSize(width: 0, height: 2)
+        view.layer.shadowRadius = 3
+        
+        return view
     }
 }
 
 
 //MARK: - Preivew
-import SwiftUI
-struct ViewControllerRepresentable: UIViewControllerRepresentable {
-    typealias UIViewControllerType = HomeViewController
-    
-    func makeUIViewController(context: Context) -> HomeViewController {
-        return HomeViewController()
-    }
-    
-    func updateUIViewController(_ uiViewController: HomeViewController, context: Context) {
-        
-    }
-}
-
-@available(iOS 13.0.0, *)
-struct ViewPreview: PreviewProvider {
-    static var previews: some View {
-        Group {
-            ViewControllerRepresentable()
-        }
-    }
-}
+//import SwiftUI
+//struct ViewControllerRepresentable: UIViewControllerRepresentable {
+//    typealias UIViewControllerType = HomeViewController
+//
+//    func makeUIViewController(context: Context) -> HomeViewController {
+//        return HomeViewController()
+//    }
+//
+//    func updateUIViewController(_ uiViewController: HomeViewController, context: Context) {
+//
+//    }
+//}
+//
+//@available(iOS 13.0.0, *)
+//struct ViewPreview: PreviewProvider {
+//    static var previews: some View {
+//        Group {
+//            ViewControllerRepresentable()
+//        }
+//    }
+//}
