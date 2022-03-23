333b87883e69f80d670c7056c18ae2749932dab1
유정주 <dbwjdwn9216@gmail.com>
2022-03-23 11:01:06 +0900

    Add: 비밀번호 TextField 추가

--- Chameleon/Chameleon/ViewControllers/LoginViewController.swift
+++ Chameleon/Chameleon/ViewControllers/LoginViewController.swift
@@ -14,7 +14,8 @@ class LoginViewController: UIViewController {
     let titleLabel: UILabel = UILabel()
     let textFieldStack: UIStackView = UIStackView()
     let idTextField: UITextField = UnderLineTextField()
-    
+    let pwTextField: UITextField = UnderLineTextField()
+
     //MARK: - Life Cycles
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,13 +24,17 @@ class LoginViewController: UIViewController {
         
     }
     
+    //MARK: - Overrides
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
     //MARK: - Methods
     private func setUpLoginUI() {
         view.backgroundColor = .systemBackground
         
         setUpTitleLabel()
         setUpTextFields()
-        
     }
     
     private func setUpTitleLabel() {
@@ -46,17 +51,17 @@ class LoginViewController: UIViewController {
     }
     
     private func setUpTextFields() {
-        setUpIdTextField()
-        
         textFieldStack.translatesAutoresizingMaskIntoConstraints = false
         textFieldStack.axis = .vertical
         textFieldStack.distribution = .fillEqually
-        textFieldStack.spacing = 20
+        textFieldStack.spacing = 30
         
+        setUpIdTextField()
+        setUpPwTextField()
         
         view.addSubview(textFieldStack)
         textFieldStack.snp.makeConstraints { make in
-            make.width.equalTo(view.safeAreaLayoutGuide).offset(-200)
+            make.width.equalTo(view.safeAreaLayoutGuide).offset(-160)
             make.height.equalTo(100)
             make.centerX.equalToSuperview()
             make.top.equalTo(titleLabel.snp.bottom).offset(40)
@@ -65,12 +70,23 @@ class LoginViewController: UIViewController {
     
     private func setUpIdTextField() {
         idTextField.placeholder = "email"
+        idTextField.clearButtonMode = .whileEditing
         
-        textFieldStack.addSubview(idTextField)
+        textFieldStack.addArrangedSubview(idTextField)
         idTextField.snp.makeConstraints { make in
             make.width.equalToSuperview()
             make.centerX.equalToSuperview()
         }
     }
     
+    private func setUpPwTextField() {
+        pwTextField.placeholder = "password"
+        pwTextField.clearButtonMode = .whileEditing
+        
+        textFieldStack.addArrangedSubview(pwTextField)
+        pwTextField.snp.makeConstraints { make in
+            make.width.equalToSuperview()
+            make.centerX.equalToSuperview()
+        }
+    }
 }
