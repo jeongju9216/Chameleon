57398486e5397b284c725b5d9080a57318c8b593
유정주 <dbwjdwn9216@gmail.com>
2022-03-29 13:25:29 +0900

    Add: Firebase SPM 추가

--- Chameleon/Chameleon/ViewControllers/MoreViewController.swift
+++ Chameleon/Chameleon/ViewControllers/MoreViewController.swift
@@ -18,14 +18,8 @@ class MoreViewController: UIViewController {
 
     func test() {
         let test = UILabel()
-        view.backgroundColor = .white // 배경색
+        view.backgroundColor = UIColor().backgroundColor() // 배경색
         view.addSubview(test)
-        
-        test.text = "MoreViewController" // test를 위해서 출력할 라벨
-        test.textColor = .blue
-        test.translatesAutoresizingMaskIntoConstraints = false
-        test.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        test.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
     }
 }
 
