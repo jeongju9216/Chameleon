5c096fe786279d2306d676ed2ba26b376098fe0c
유정주 <dbwjdwn9216@gmail.com>
2022-03-30 09:46:17 +0900

    Add: LoadingVIewController 추가

---
+++ Chameleon/Chameleon/ViewControllers/LoadingViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LoadingViewController.swift
+//  Chameleon
+//
+//  Created by 유정주 on 2022/03/30.
+//
+
+import UIKit
+import SnapKit
+
+class LoadingViewController: BaseViewController {
+
+    //MARK: - Views
+    
+    //MARK: - Life Cycles
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
