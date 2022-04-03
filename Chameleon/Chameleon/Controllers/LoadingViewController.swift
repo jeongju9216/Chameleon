c10bb1365eb3e6b815decfbdbd7e77168fce43b4
유정주 <dbwjdwn9216@gmail.com>
2022-04-03 17:33:46 +0900

    Update: Loading VC Snapkit -> Constraint 수정

--- Chameleon/Chameleon/Controllers/LoadingViewController.swift
+++ Chameleon/Chameleon/Controllers/LoadingViewController.swift
@@ -11,20 +11,15 @@ import Lottie
 class LoadingViewController: BaseViewController {
     
     //MARK: - Views
-    let guideLabel: UILabel = UILabel()
+    var guideLabel: UILabel!
     
-    let chameleonImageView: UIImageView = UIImageView()
-    lazy var topAnimationView: AnimationView = {
-        UITraitCollection.current.userInterfaceStyle == .light ? .init(name: "bottomImage-Light")
-        : .init(name: "bottomImage-Dark")
-    }()
-    lazy var bottomAnimationView: AnimationView = {
-        UITraitCollection.current.userInterfaceStyle == .light ? .init(name: "bottomImage-Light")
-        : .init(name: "bottomImage-Dark")
-    }()
+    var chameleonImageView: UIImageView!
+    var topAnimationView: AnimationView!
+    var bottomAnimationView: AnimationView!
     
     //MARK: - Properties
     var guideString: String = ""
+    var animationName = UITraitCollection.current.userInterfaceStyle == .light ? "bottomImage-Light" : "bottomImage-Dark"
     
     //MARK: - Life Cycles
     override func viewDidLoad() {
@@ -33,7 +28,7 @@ class LoadingViewController: BaseViewController {
         setupLoadingUI()
         
         //test code
-        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 1) {
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 3) {
             self.dismissLoading()
         }
     }
@@ -53,6 +48,15 @@ class LoadingViewController: BaseViewController {
         }
     }
     
+    private func animationLoadingTimer() {
+        var count = 0
+        let timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+            self.guideLabel.text = self.guideString + String(repeating: ".", count: count % 3)
+            count += 1
+        }
+        timer.fire()
+    }
+    
     //MARK: - Setup
     private func setupLoadingUI() {
         view.backgroundColor = UIColor.backgroundColor
@@ -64,41 +68,42 @@ class LoadingViewController: BaseViewController {
     }
     
     private func setupGuideLabel() {
+        guideLabel = UILabel()
+        guideLabel.translatesAutoresizingMaskIntoConstraints = false
+        
         guideLabel.text = guideString
         guideLabel.font = UIFont.boldSystemFont(ofSize: 32)
         guideLabel.numberOfLines = 0
         
-        var count = 0
-        let timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
-            self.guideLabel.text = self.guideString + String(repeating: ".", count: count % 3)
-            count += 1
-        }
-        timer.fire()
+        animationLoadingTimer()
         
         view.addSubview(guideLabel)
-        guideLabel.snp.makeConstraints { make in
-            make.top.equalTo(chameleonImageView.snp.bottom).offset(10)
-            make.centerX.equalToSuperview()
-        }
+        guideLabel.topAnchor.constraint(equalTo: chameleonImageView.bottomAnchor, constant: 10).isActive = true
+        guideLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
     }
     
     private func setupChameleonImage() {
+        chameleonImageView = UIImageView()
+        chameleonImageView.translatesAutoresizingMaskIntoConstraints = false
+        
         if let chameleonImage = UIImage(named: "ChameleonImage") {
             chameleonImageView.image = chameleonImage
             chameleonImageView.contentMode = .scaleAspectFill
             chameleonImageView.layer.allowsEdgeAntialiasing = true
             
             view.addSubview(chameleonImageView)
-            chameleonImageView.snp.makeConstraints { make in
-                make.width.height.equalTo(70)
-                make.centerX.equalToSuperview()
-                make.centerY.equalToSuperview().offset(-40)
-            }
+            chameleonImageView.widthAnchor.constraint(equalToConstant: 70).isActive = true
+            chameleonImageView.heightAnchor.constraint(equalToConstant: 70).isActive = true
+            chameleonImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+            chameleonImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -40).isActive = true
         }
         
     }
     
     private func setupTopAnimationView() {
+        topAnimationView = .init(name: animationName)
+        topAnimationView.translatesAutoresizingMaskIntoConstraints = false
+        
         topAnimationView.transform = .init(rotationAngle: .pi)
         
         topAnimationView.contentMode = .scaleAspectFill
@@ -108,15 +113,16 @@ class LoadingViewController: BaseViewController {
         topAnimationView.play()
         
         view.addSubview(topAnimationView)
-        topAnimationView.snp.makeConstraints { make in
-            make.centerX.equalToSuperview()
-            make.width.equalTo(view.frame.width)
-            make.height.equalTo(130)
-            make.top.equalTo(view.snp.top).offset(-20)
-        }
+        topAnimationView.widthAnchor.constraint(equalToConstant: view.frame.width).isActive = true
+        topAnimationView.heightAnchor.constraint(equalToConstant: 130).isActive = true
+        topAnimationView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        topAnimationView.topAnchor.constraint(equalTo: view.topAnchor, constant: -20).isActive = true
     }
     
     private func setupBottomAnimationView() {
+        bottomAnimationView = .init(name: animationName)
+        bottomAnimationView.translatesAutoresizingMaskIntoConstraints = false
+        
         bottomAnimationView.contentMode = .scaleAspectFill
         bottomAnimationView.loopMode = .loop
         bottomAnimationView.animationSpeed = 1.2
@@ -124,11 +130,9 @@ class LoadingViewController: BaseViewController {
         bottomAnimationView.play()
         
         view.addSubview(bottomAnimationView)
-        bottomAnimationView.snp.makeConstraints { make in
-            make.centerX.equalToSuperview()
-            make.width.equalTo(view.frame.width)
-            make.height.equalTo(130)
-            make.bottom.equalTo(view.snp.bottom).offset(20)
-        }
+        bottomAnimationView.widthAnchor.constraint(equalToConstant: view.frame.width).isActive = true
+        bottomAnimationView.heightAnchor.constraint(equalToConstant: 130).isActive = true
+        bottomAnimationView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        bottomAnimationView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 20).isActive = true
     }
 }
