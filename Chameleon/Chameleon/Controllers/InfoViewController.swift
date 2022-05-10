0668a29525570a4745eac244b873af606d5a67f7
유정주 <dbwjdwn9216@gmail.com>
2022-05-10 14:04:55 +0900

    Add: 앱 정보 화면 추가

---
+++ Chameleon/Chameleon/Controllers/InfoViewController.swift
@@ -0,0 +1,25 @@
+//
+//  InfoViewController.swift
+//  Chameleon
+//
+//  Created by 유정주 on 2022/05/10.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController {
+    //MARK: - Views
+    
+    
+    //MARK: - Life Cycles
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    //MARK: - Actions
+    
+    //MARK: - Methods
+    private func setupInfoView() {
+        
+    }
+}
