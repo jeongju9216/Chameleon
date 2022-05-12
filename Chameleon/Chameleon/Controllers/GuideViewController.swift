18a5d1afa9ca48163bb7cb76c9686201b06f3d9f
유정주 <dbwjdwn9216@gmail.com>
2022-05-12 17:34:18 +0900

    Add: 도움말 화면 추가

---
+++ Chameleon/Chameleon/Controllers/GuideViewController.swift
@@ -0,0 +1,35 @@
+//
+//  GuideViewController.swift
+//  Chameleon
+//
+//  Created by 유정주 on 2022/05/12.
+//
+
+import UIKit
+
+class GuideViewController: BaseViewController {
+
+    //MARK: - Views
+    private var logoImageView: UIImageView!
+    private var guideLabel: UILabel!
+    
+    //MARK: - Life Cycles
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupGuideUI()
+    }
+    
+    //MARK: - Methods
+    private func setupGuideUI() {
+        
+    }
+    
+    private func setupLogoImageView() {
+        
+    }
+    
+    private func setupGuideLabel() {
+        
+    }
+}
