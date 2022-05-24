f78a419ec8ecc4d0df5aa7650b3b931f5ee13734
유정주 <dbwjdwn9216@gmail.com>
2022-05-24 22:45:11 +0900

    Remove: cancel api 삭제

--- Chameleon/Chameleon/ConvertScreen/ConvertViewController.swift
+++ Chameleon/Chameleon/ConvertScreen/ConvertViewController.swift
@@ -55,14 +55,7 @@ class ConvertViewController: BaseViewController {
             self.navigationController?.pushViewController(resultVC, animated: true)
         } else {
             self.showTwoButtonAlert(message: "얼굴 변환을 중단하시겠습니까?", defaultButtonTitle: "중단하기", cancelButtonTitle: "이어하기", defaultAction: { _ in
-                LoadingIndicator.showLoading()
-                HttpService.shared.cancelFiles(completionHandler: { [weak self] result, response in
-                    LoadingIndicator.hideLoading()
-                    
-                    DispatchQueue.main.async {
-                        self?.navigationController?.popViewController(animated: true)
-                    }
-                })
+                self.navigationController?.popViewController(animated: true)
             })
         }
     }
