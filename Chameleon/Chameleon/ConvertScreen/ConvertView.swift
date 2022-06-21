e33699cff3dc20cfada349fe075a7584093e026f
유정주 <dbwjdwn9216@gmail.com>
2022-06-21 22:55:43 +0900

    Refactor: ConvertVC에서 View 분리

---
+++ Chameleon/Chameleon/ConvertScreen/ConvertView.swift
@@ -0,0 +1,145 @@
+//
+//  ConvertView.swift
+//  Chameleon
+//
+//  Created by 유정주 on 2022/06/21.
+//
+
+import UIKit
+
+final class ConvertView: UIView {
+    
+    //MARK: - Views
+    var guideLabel: UILabel!
+    
+    var uploadImageView: UIImageView!
+    
+    var progressStack: UIStackView!
+    var progressLabel: UILabel!
+    var progressView: UIProgressView!
+    
+    var doneButton: UIButton!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setup()
+    }
+    
+    //MARK: - Methods
+    func setCompleteText() {
+        doneButton.setTitle("결과 보기", for: .normal)
+        guideLabel.text = "얼굴 변환이 완료되었습니다.\n결과를 확인하세요."
+    }
+    
+    func setProgressTime(time: Int) {
+        progressView.setProgress(Float(Double(time) / 100.0), animated: true)
+        progressLabel.text = "\(min(time, 100))%"
+    }
+    
+    //MARK: - Setup
+    private func setup() {
+        self.backgroundColor = .backgroundColor
+        
+        setupUploadView()
+        setupGuideLabel()
+
+        setupProgressStack()
+        setupProgressLabel()
+        setupProgressView()
+        
+        setupDoneButton()
+    }
+    
+    private func setupGuideLabel() {
+        guideLabel = UILabel()
+        guideLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        guideLabel.text = "얼굴 변환이 진행 중입니다.\n잠시만 기다려 주세요."
+        guideLabel.font = UIFont.systemFont(ofSize: 18)
+        guideLabel.textAlignment = .center
+        guideLabel.numberOfLines = 0
+        
+        self.addSubview(guideLabel)
+        guideLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+        guideLabel.bottomAnchor.constraint(equalTo: uploadImageView.topAnchor, constant: -40).isActive = true
+    }
+    
+    private func setupUploadView() {
+        uploadImageView = UIImageView()
+        uploadImageView.image = UIImage(named: "ChameleonImage")
+        uploadImageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        uploadImageView.backgroundColor = UIColor.backgroundColor
+        
+        uploadImageView.clipsToBounds = true
+        uploadImageView.layer.cornerRadius = 20
+        
+        self.addSubview(uploadImageView)
+        uploadImageView.widthAnchor.constraint(equalToConstant: 70).isActive = true
+        uploadImageView.heightAnchor.constraint(equalToConstant: 70).isActive = true
+        uploadImageView.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+        uploadImageView.centerYAnchor.constraint(equalTo: self.centerYAnchor, constant: -40).isActive = true
+    }
+    
+    private func setupProgressStack() {
+        progressStack = UIStackView()
+        progressStack.translatesAutoresizingMaskIntoConstraints = false
+        
+        progressStack.axis = .vertical
+        progressStack.spacing = 10
+        progressStack.alignment = .center
+        progressStack.distribution = .fill
+        
+        let width = min(self.frame.width * 0.6, 500)
+        self.addSubview(progressStack)
+        progressStack.widthAnchor.constraint(equalToConstant: width).isActive = true
+        progressStack.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+        progressStack.topAnchor.constraint(equalTo: uploadImageView.bottomAnchor, constant: 20).isActive = true
+    }
+    
+    private func setupProgressLabel() {
+        progressLabel = UILabel()
+        progressLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        progressLabel.text = "0%"
+        progressLabel.font = UIFont.systemFont(ofSize: 14)
+        progressLabel.textColor = .lightGray
+        
+        progressStack.addArrangedSubview(progressLabel)
+    }
+    
+    private func setupProgressView() {
+        progressView = UIProgressView()
+        progressView.translatesAutoresizingMaskIntoConstraints = false
+        
+        progressView.clipsToBounds = true
+        progressView.layer.cornerRadius = 5
+        
+        progressView.trackTintColor = .lightGray
+        progressView.progressTintColor = .mainColor
+        progressView.progress = 0
+        
+        progressStack.addArrangedSubview(progressView)
+        progressView.widthAnchor.constraint(equalTo: progressStack.widthAnchor).isActive = true
+        progressView.heightAnchor.constraint(equalToConstant: 10).isActive = true
+    }
+    
+    private func setupDoneButton() {
+        doneButton = UIButton()
+        doneButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        doneButton.applyMainButtonStyle(title: "변환 중단")
+        
+        let width = min(self.frame.width - 80, 800)
+        self.addSubview(doneButton)
+        doneButton.widthAnchor.constraint(equalToConstant: width).isActive = true
+        doneButton.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        doneButton.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+        doneButton.bottomAnchor.constraint(equalTo: self.safeAreaLayoutGuide.bottomAnchor, constant: -20).isActive = true
+    }
+}
--- Chameleon/Chameleon/ConvertScreen/ConvertViewController.swift
+++ Chameleon/Chameleon/ConvertScreen/ConvertViewController.swift
@@ -10,15 +10,7 @@ import UIKit
 class ConvertViewController: BaseViewController {
 
     //MARK: - Views
-    var guideLabel: UILabel!
-    
-    var uploadImageView: UIImageView!
-    
-    var progressStack: UIStackView!
-    var progressLabel: UILabel!
-    var progressView: UIProgressView!
-    
-    var doneButton: UIButton!
+    private var convertView: ConvertView!
     
     //MARK: - Properties
     var resultURL: String?
@@ -31,14 +23,20 @@ class ConvertViewController: BaseViewController {
     //MARK: - Life Cycles
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupNavigationBar(title: "얼굴 변환 중")
         
-        setupCovertUI()
-        
-        doneButton.addTarget(self, action: #selector(clickedDoneButton(sender:)), for: .touchUpInside)
+        convertView.doneButton.addTarget(self, action: #selector(clickedDoneButton(sender:)), for: .touchUpInside)
         
         firstStandTime = Int.random(in: 10...20)
         timer?.invalidate()
         timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(progressConvert(sender:)), userInfo: nil, repeats: true)
+    }
+    
+    override func loadView() {
+        super.loadView()
+        
+        convertView = ConvertView(frame: self.view.frame)
+        self.view = convertView
     }
     
     override func viewDidDisappear(_ animated: Bool) {
@@ -119,8 +117,7 @@ class ConvertViewController: BaseViewController {
             }
         }
         
-        progressView.setProgress(Float(Double(time) / 100.0), animated: true)
-        progressLabel.text = "\(min(time, 100))%"
+        convertView.setProgressTime(time: time)
     }
     
     private func errorEvent() {
@@ -128,113 +125,7 @@ class ConvertViewController: BaseViewController {
     }
     
     private func completeConvert() {
-        doneButton.setTitle("결과 보기", for: .normal)
-        
-        guideLabel.text = "얼굴 변환이 완료되었습니다.\n결과를 확인하세요."
-        
+        convertView.setCompleteText()
         setupNavigationBar(title: "얼굴 변환 완료")
-    }
-    
-    //MARK: - Setup
-    private func setupCovertUI() {
-        setupNavigationBar(title: "얼굴 변환 중")
-        view.backgroundColor = .backgroundColor
-        
-        setupUploadView()
-        setupGuideLabel()
-
-        setupProgressStack()
-        setupProgressLabel()
-        setupProgressView()
-        
-        setupDoneButton()
-    }
-    
-    private func setupGuideLabel() {
-        guideLabel = UILabel()
-        guideLabel.translatesAutoresizingMaskIntoConstraints = false
-        
-        guideLabel.text = "얼굴 변환이 진행 중입니다.\n잠시만 기다려 주세요."
-        guideLabel.font = UIFont.systemFont(ofSize: 18)
-        guideLabel.textAlignment = .center
-        guideLabel.numberOfLines = 0
-        
-        view.addSubview(guideLabel)
-        guideLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        guideLabel.bottomAnchor.constraint(equalTo: uploadImageView.topAnchor, constant: -40).isActive = true
-    }
-    
-    private func setupUploadView() {
-        uploadImageView = UIImageView()
-        uploadImageView.image = UIImage(named: "ChameleonImage")
-        uploadImageView.translatesAutoresizingMaskIntoConstraints = false
-        
-        uploadImageView.backgroundColor = UIColor.backgroundColor
-        
-        uploadImageView.clipsToBounds = true
-        uploadImageView.layer.cornerRadius = 20
-        
-        view.addSubview(uploadImageView)
-        uploadImageView.widthAnchor.constraint(equalToConstant: 70).isActive = true
-        uploadImageView.heightAnchor.constraint(equalToConstant: 70).isActive = true
-        uploadImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        uploadImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -40).isActive = true
-    }
-    
-    private func setupProgressStack() {
-        progressStack = UIStackView()
-        progressStack.translatesAutoresizingMaskIntoConstraints = false
-        
-        progressStack.axis = .vertical
-        progressStack.spacing = 10
-        progressStack.alignment = .center
-        progressStack.distribution = .fill
-        
-        let width = min(view.frame.width * 0.6, 500)
-        view.addSubview(progressStack)
-        progressStack.widthAnchor.constraint(equalToConstant: width).isActive = true
-        progressStack.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        progressStack.topAnchor.constraint(equalTo: uploadImageView.bottomAnchor, constant: 20).isActive = true
-    }
-    
-    private func setupProgressLabel() {
-        progressLabel = UILabel()
-        progressLabel.translatesAutoresizingMaskIntoConstraints = false
-        
-        progressLabel.text = "0%"
-        progressLabel.font = UIFont.systemFont(ofSize: 14)
-        progressLabel.textColor = .lightGray
-        
-        progressStack.addArrangedSubview(progressLabel)
-    }
-    
-    private func setupProgressView() {
-        progressView = UIProgressView()
-        progressView.translatesAutoresizingMaskIntoConstraints = false
-        
-        progressView.clipsToBounds = true
-        progressView.layer.cornerRadius = 5
-        
-        progressView.trackTintColor = .lightGray
-        progressView.progressTintColor = .mainColor
-        progressView.progress = 0
-        
-        progressStack.addArrangedSubview(progressView)
-        progressView.widthAnchor.constraint(equalTo: progressStack.widthAnchor).isActive = true
-        progressView.heightAnchor.constraint(equalToConstant: 10).isActive = true
-    }
-    
-    private func setupDoneButton() {
-        doneButton = UIButton()
-        doneButton.translatesAutoresizingMaskIntoConstraints = false
-        
-        doneButton.applyMainButtonStyle(title: "변환 중단")
-        
-        let width = min(view.frame.width - 80, 800)
-        view.addSubview(doneButton)
-        doneButton.widthAnchor.constraint(equalToConstant: width).isActive = true
-        doneButton.heightAnchor.constraint(equalToConstant: 40).isActive = true
-        doneButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        doneButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20).isActive = true
     }
 }
